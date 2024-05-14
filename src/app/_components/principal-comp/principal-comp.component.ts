8c3e99380450617cc6aeb37b443d64ac9f24f6e8
Fabricio <fabriciodev2023@gmail.com>
2024-05-13 21:36:47 -0300

    primeiro commit

---
+++ src/app/_components/principal-comp/principal-comp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-principal-comp',
+  templateUrl: './principal-comp.component.html',
+  styleUrl: './principal-comp.component.css'
+})
+export class PrincipalCompComponent {
+
+}
